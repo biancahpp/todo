0a559c384578f7958424d8239bb638a04e41b7eb
biancahpp <bianca_procopio@hotmail.com>
2020-12-08 16:39:16 -0600

    completed working

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,20 +1,60 @@
 import React from 'react';
-import './styles.css'
+import './styles.css';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 
 export default function List() {
   
   const [ list, setList] = useState([]);
 
+  const [ item, setItem ] = useState(null);
+
   function addItem (e) {
+    e.preventDefault()
+    setList([...list, {item: item, completed: false}])
+    console.log(list)
+  }
+
+  function deleteItem (index) {
+    const deleted = list.filter((_, ind) => {
+      return ind !== index
+    });
+    setList(deleted);
+  }
+
+  function completeTodo(index) {
     
+    const res = list.map((el, ind) => {
+
+      return ind === index ? { ...el, completed: true }: el;
+    }
+    )
+    setList(res)
   }
 
   return (
     <div className='listWrapper'>
-      <header>
-      <input type='text' placeholder='Create a new todo...'></input>
-      </header>
+      <form onSubmit={addItem}>
+        <input type='text' placeholder='Create a new todo...' onChange={(e) => {
+         setItem(e.target.value); }} >
+         </input>
+         <button type='submit'>ICON</button>
+      </form>
+      <div className='todoListContainer'>
+        {list ? 
+        list.map((item, index) => 
+        <div key={uuidv4()}className='todoItem'>
+          <button className='complete' onClick={ () => completeTodo(index)}>
+            C
+          </button>
+          {item.item}
+          <button onClick={() => deleteItem(index)}>
+            Delete
+          </button>
+          </div>) : 
+        <div> Loading </div>}
+      </div>
       
     </div>
   )
